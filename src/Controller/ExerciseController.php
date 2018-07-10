13e35e7a8d8dc4de503625280a06fce0e401c3d6
daddyStan <sts.ko@mail.ru>
2018-07-11 01:40:56 +0300

    mtr-100720180329 css

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -2,18 +2,66 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ExerciseRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ExerciseController extends Controller
 {
+    /** @var ExerciseRepository */
+    private $exerciseRepository;
+
+    /** @var EntityManager */
+    private $em;
+
     /**
-     * @Route("/exercise", name="exercise")
+     * @return mixed
      */
-    public function index()
+    public function getExerciseRepository()
+    {
+        return $this->exerciseRepository;
+    }
+
+    /**
+     * @param $exerciseRepository
+     * @return ExerciseController
+     */
+    public function setExerciseRepository($exerciseRepository): self
+    {
+        $this->exerciseRepository = $exerciseRepository;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEm()
+    {
+        return $this->em;
+    }
+
+    /**
+     * @param $em
+     * @return ExerciseController
+     */
+    public function setEm($em): self
+    {
+        $this->em = $em;
+        return $this;
+    }
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->setEm($em)
+             ->setExerciseRepository($this->em->getRepository('App:Exercise'));
+    }
+
+    public function index($id)
     {
         return $this->render('exercise/index.html.twig', [
             'controller_name' => 'ExerciseController',
+            'id'              => $id
         ]);
     }
 }
